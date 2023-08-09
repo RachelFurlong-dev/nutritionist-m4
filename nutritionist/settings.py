2a45ebf3b1e8a3a49e4efc6e94ff5484df8c5521
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2023-08-09 16:36:08 +0000

    Remove secret key and set debug

--- nutritionist/settings.py
+++ nutritionist/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c_z-ujun@e(+q9v&*na3u&4p2xrq68100i(0c3k7x$_kqfp79x'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nutritionist-rf-f7d23ec1a841.herokuapp.com', 'nutritionist-rf.herokuapp.com', 'localhost']
 
