22954a0162d4201d4f78219ed6e12ff134cb2774
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2023-09-17 14:16:49 +0000

    update blog views and templates

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
 class Post(models.Model):
     post_title = models.CharField(max_length=300, blank=False, null=False)
     slug = models.SlugField(max_length=50)
@@ -9,6 +15,7 @@ class Post(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(null=True, blank = True) 
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return self.post_title
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_blog, name='blog'),
+    path('', views.PostList.as_view(), name='post_list'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
