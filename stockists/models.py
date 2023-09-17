da83afd648c3e0498e890681e645de88cfd23cd5
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2023-09-17 12:55:46 +0000

    Update stockist fields

--- stockists/models.py
+++ stockists/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Stockist(models.Model):
-    business_type = models.CharField(max_length=50, null=False, blank=False)
-    in_stock = models.BooleanField(null=False, blank=False, default=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    has_stock = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
-        return self.business_type
+        return self.name
