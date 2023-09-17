f4ac589c1f5c2f2d2fe8f695e1a21c58abef1f95
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2023-09-17 15:09:22 +0000

    Update blog admin file fields

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,9 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('post_title', 'body', 'publish_date', 'thumb', 'author', 'status',)
-
+    list_display = ('post_title', 'slug', 'publish_date', 'status',)
+    list_filter = ("status",)
+    search_fields = ['post_title', 'body']
+    prepopulated_fields = {'slug': ('post_title',)}
 
 admin.site.register(Post, PostAdmin)
