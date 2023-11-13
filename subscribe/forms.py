0b51e21ed8b1f05dad24543d1fef47680dd552e3
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2023-11-13 13:06:44 +0000

    Setup views, urls, forms and admin in subscribe app

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,7 +1,8 @@
-"""from django import forms
+from django import forms
 from .models import Subscriber
 
 class SubscribeForm(forms.ModelForm):
     class Meta:
         model = Subscriber
-        fields = ['email']"""
+        fields = ['email']
+        
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,4 +1,5 @@
-"""from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import SubscribeForm
 
 def subscribe(request):
@@ -6,10 +7,13 @@ def subscribe(request):
         form = SubscribeForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'You have successfully subscribed!')
             return redirect('subscribe_success')
+        else:
+            messages.error(request, 'Invalid email address. Please try again.')
     else:
         form = SubscribeForm()
     return render(request, 'newsletter/subscribe.html', {'form': form})
 
 def subscribe_success(request):
-    return render(request, 'newsletter/subscribe_success.html')"""
+    return render(request, 'newsletter/subscribe_success.html')
