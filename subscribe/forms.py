37dcdec265e8fca9499ed4125e481661d2d6a795
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2023-11-15 10:31:14 +0000

    Add form fields to forms.py to connect with bootstrap form

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -5,3 +5,25 @@ class SubscribeForm(forms.ModelForm):
     class Meta:
         model = Subscriber
         fields = ('name', 'email',)
+
+def __init__(self, *args, **kwargs):
+        """add placeholders and classes """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email',
+        }
+
+        """autofocus and aria attributes"""
+        self.fields['name'].widget.attrs['autofocus'] = True
+        self.fields['name'].widget.attrs['aria-label'] = 'Name'
+        self.fields['email'].widget.attrs['aria-label'] = 'Email'
+        
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black'
+            self.fields[field].label = False
