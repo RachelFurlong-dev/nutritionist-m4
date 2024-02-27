63f7fd149d29a52ebf373c88500eea5e3d86afd9
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2024-02-27 12:37:48 +0000

    Add import statement to profile views

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
+from django.shortcuts import get_object_or_404
 
 from checkout.models import Order
 
